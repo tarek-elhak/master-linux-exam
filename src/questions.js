c53d093aeb16658cbda389ed37bcc561d8f26d3c
tarek-elhak <tarekroja123@gmail.com>
2022-05-27 17:30:38 +0200

    prepare the questions data

---
+++ src/questions.js
@@ -0,0 +1,64 @@
+export default [
+  {
+    question: "Whistler was the codename of this Microsoft Operating System.",
+    correct_answer: "Windows XP",
+    answers: ["Windows 2000", "Windows 7", "Windows 95", "Windows XP"],
+  },
+  {
+    question:
+      "What does AD stand for in relation to Windows Operating Systems? ",
+    correct_answer: "Active Directory",
+    answers: [
+      "Alternative Drive",
+      "Automated Database",
+      "Active Department",
+      "Active Directory",
+    ],
+  },
+  {
+    question: "On which computer hardware device is the BIOS chip located?",
+    correct_answer: "Motherboard",
+    answers: [
+      "Hard Disk Drive",
+      "Central Processing Unit",
+      "Graphics Processing Unit",
+      "Motherboard",
+    ],
+  },
+  {
+    question: "What does the acronym CDN stand for in terms of networking?",
+    correct_answer: "Content Delivery Network",
+    answers: [
+      "Content Distribution Network",
+      "Computational Data Network",
+      "Compressed Data Network",
+      "Content Delivery Network",
+    ],
+  },
+  {
+    question:
+      "In HTML, which non-standard tag used to be be used to make elements scroll across the viewport?",
+    correct_answer: "&lt;marquee&gt;&lt;/marquee&gt;",
+    answers: [
+      "&lt;scroll&gt;&lt;/scroll&gt;",
+      "&lt;move&gt;&lt;/move&gt;",
+      "&lt;slide&gt;&lt;/slide&gt;",
+      "&lt;marquee&gt;&lt;/marquee&gt;",
+    ],
+  },
+  {
+    question: ".at is the top-level domain for what country?",
+    correct_answer: "Austria",
+    answers: ["Argentina", "Australia", "Angola", "Austria"],
+  },
+  {
+    question: "What does RAID stand for?",
+    correct_answer: "Redundant Array of Independent Disks",
+    answers: [
+      "Rapid Access for Indexed Devices",
+      "Range of Applications with Identical Designs",
+      "Randomized Abstract Identification Description",
+      "Redundant Array of Independent Disks",
+    ],
+  },
+];
