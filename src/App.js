3b6399515750a395b401f4230763df2c819a7b74
tarek-elhak <tarekroja123@gmail.com>
2022-05-28 17:25:54 +0200

    adding the not found page with refine the application routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import Auth from "./containers/Auth/Auth";
 import Exam from "./containers/Exam/Exam";
 import ExamResult from "./components/ExamResult/ExamResult";
-import { useEffect } from "react";
+import NotFound from "./components/NotFound/NotFound";
+// import { useEffect } from "react";
 import * as actionCreators from "./store/actions/index";
 
 const App = (props) => {
-  const { loginUser } = props;
   useEffect(() => {
     const authenticated = localStorage.getItem("isAuthenticated");
 
     if (authenticated) {
-      loginUser();
+      props.loginUser();
     }
-  }, [loginUser]);
+  }, []);
 
-  let routes;
-
-  if (props.isAuthenticated) {
-    routes = (
+  return (
+    <div className="App">
       <Routes>
-        <Route path="/" element={<Navigate to="/exam" replace />} />
-        <Route path="/login" element={<Navigate to="/exam" replace />} />
-        <Route path="/exam" element={<Exam />} />
-        <Route path="/exam-result" element={<ExamResult />} />
+        {props.isAuthenticated ? (
+          <>
+            <Route path="/exam" element={<Exam />} />
+            <Route path="/exam-result" element={<ExamResult />} />
+          </>
+        ) : (
+          <Route path="/login" element={<Auth />} />
+        )}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    );
-  } else {
-    routes = (
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/exam" element={<Navigate to="/login" replace />} />
-        <Route path="/exam-result" element={<Navigate to="/login" replace />} />
-      </Routes>
-    );
-  }
-  return <div className="App">{routes}</div>;
+    </div>
+  );
 };
 
 const mapDispatchToProps = (dispatch) => {
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,3 @@
+const NotFound = (props) => <h1>The requested page is Not Found - 404 </h1>;
+
+export default NotFound;
