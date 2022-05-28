03505a09a52a64ab5facbccabf26111cdf19d967
tarek-elhak <tarekroja123@gmail.com>
2022-05-28 19:53:21 +0200

    refine the Auth component and remove all duplications

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import * as actionCreators from "../../store/actions/index";
@@ -8,175 +8,108 @@ import classes from "./Auth.module.css";
 
 const Auth = (props) => {
   const navigate = useNavigate();
-  const [name, setName] = useState({
-    value: "",
-    rules: { required: true, min: 3 },
-    error: "",
-    touched: false,
-  });
-  const [email, setEmail] = useState({
-    value: "",
-    rules: { required: true, type: "email", error: "" },
-    error: "",
-    touched: false,
-  });
-  const [password, setPassword] = useState({
-    value: "",
-    rules: {
-      required: true,
-      min: 8,
-      includes: { number: true, uppercase: true },
+  const [input, setInput] = useState({
+    name: {
+      value: "",
+      rules: { required: true, min: 3 },
+      error: "",
+      hasError: true,
+      touched: false,
     },
-    error: "",
-    touched: false,
+    email: {
+      value: "",
+      rules: { required: true, email: true, error: "" },
+      error: "",
+      hasError: true,
+      touched: false,
+    },
+    password: {
+      value: "",
+      rules: {
+        required: true,
+        min: 8,
+        includes: { number: true, uppercase: true },
+      },
+      error: "",
+      hasError: true,
+      touched: false,
+    },
   });
 
-  const [hasError, setHasError] = useState(true);
-  useEffect(() => {
-    if (
-      name.touched &&
-      !name.error &&
-      email.touched &&
-      !email.error &&
-      password.touched &&
-      !password.error
-    ) {
-      setHasError(false);
-    }
-  }, [
-    name.touched,
-    name.error,
-    email.touched,
-    email.error,
-    password.touched,
-    password.error,
-  ]);
+  const [formValid, setFormValid] = useState(false);
 
-  const validateName = (value) => {
+  const validateForm = (type, value) => {
     let error = false;
     let errorMessage = "";
 
-    if (name.rules.required) {
+    if (input[type].rules.required) {
       if (value.trim().length === 0) {
         error = true;
-        errorMessage = "Name mustn't be empty";
+        errorMessage = `${type} mustn't be empty`;
       }
     }
-    if (!error && name.rules.min) {
-      if (value.trim().length < name.rules.min) {
+    if (!error && input[type].rules.min) {
+      if (value.trim().length < input[type].rules.min) {
         error = true;
-        errorMessage = `Name must be at least ${name.rules.min} characters`;
+        errorMessage = `${type} must be at least ${input[type].rules.min} characters`;
       }
     }
-    return { error, errorMessage };
-  };
-
-  const validateEmail = (value) => {
-    let error = false;
-    let errorMessage = "";
-
-    if (email.rules.required) {
-      if (value.trim().length === 0) {
-        error = true;
-        errorMessage = "Email mustn't be empty";
-      }
-    }
-    if (!error && email.rules.type === "email") {
+    if (!error && input[type].rules.email) {
       if (!value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
         error = true;
-        errorMessage = "You must enter a valid email address";
+        errorMessage = `You must enter a valid ${type} address`;
       }
     }
-    return { error, errorMessage };
-  };
-
-  const validatePassword = (value) => {
-    let error = false;
-    let errorMessage = "";
-
-    if (password.rules.required) {
-      if (value.trim().length === 0) {
-        error = true;
-        errorMessage = "Password mustn't be empty";
-      }
-    }
-    if (!error && password.rules.min) {
-      if (value.trim().length < password.rules.min) {
-        error = true;
-        errorMessage = `Password must be at least ${password.rules.min} characters`;
+    if (!error && input[type].rules.includes) {
+      {
+        if (input[type].rules.includes.number) {
+          if (!value.match(/\d/)) {
+            error = true;
+            errorMessage = `${type} must contains at least 1 number`;
+          }
+        }
       }
     }
 
-    if (!error && password.rules.includes.number) {
-      if (!value.match(/\d/)) {
-        error = true;
-        errorMessage = "password must contains at least 1 number";
-      }
-    }
-
-    if (!error && password.rules.includes.uppercase) {
-      if (!value.match(/[A-Z]/)) {
-        error = true;
-        errorMessage = "password must contains at least 1 uppercase letter";
+    if (!error && input[type].rules.includes) {
+      {
+        if (input[type].rules.includes.uppercase) {
+          if (!value.match(/[A-Z]/)) {
+            error = true;
+            errorMessage = `${type} must contains at least 1 uppercase letter`;
+          }
+        }
       }
     }
 
     return { error, errorMessage };
   };
 
-  const nameChangedHandler = (event) => {
-    const { error, errorMessage } = validateName(event.target.value);
-    if (error) {
-      setName((prevName) => ({
-        ...prevName,
-        error: errorMessage,
-        value: event.target.value,
-        touched: true,
-      }));
-    } else {
-      setName((prevName) => ({
-        ...prevName,
-        error: "",
-        value: event.target.value,
-        touched: true,
-      }));
+  const inputChangedHandler = (event) => {
+    const { error, errorMessage } = validateForm(
+      event.target.name,
+      event.target.value
+    );
+    const updatedInput = { ...input };
+    updatedInput[event.target.name] = {
+      ...input[event.target.name],
+      error: error ? errorMessage : "",
+      hasError: error ? true : false,
+      value: event.target.value,
+      touched: true,
+    };
+    setInput(updatedInput);
+
+    // check if the overall form inputs are valid or not
+    let formValidity = true;
+
+    for (let field in updatedInput) {
+      if (updatedInput[field].hasError) {
+        formValidity = false;
+      }
     }
-  };
-  const emailChangedHandler = (event) => {
-    const { error, errorMessage } = validateEmail(event.target.value);
-    if (error) {
-      setEmail((prevName) => ({
-        ...prevName,
-        error: errorMessage,
-        value: event.target.value,
-        touched: true,
-      }));
-    } else {
-      setEmail((prevName) => ({
-        ...prevName,
-        error: "",
-        value: event.target.value,
-        touched: true,
-      }));
-    }
-  };
-  const passwordChangedHandler = (event) => {
-    const { error, errorMessage } = validatePassword(event.target.value);
-    if (error) {
-      setPassword((prevName) => ({
-        ...prevName,
-        error: errorMessage,
-        value: event.target.value,
-        touched: true,
-      }));
-    } else {
-      setPassword((prevName) => ({
-        ...prevName,
-        error: "",
-        value: event.target.value,
-        touched: true,
-      }));
-    }
+
+    setFormValid(formValidity);
   };
 
   const authSubmitHandler = (event) => {
@@ -190,12 +123,13 @@ const Auth = (props) => {
         <input
           placeholder="Name"
           type="text"
-          value={name.value}
-          onChange={nameChangedHandler}
+          name="name"
+          value={input.name.value}
+          onChange={inputChangedHandler}
         />
 
-        {name.error.length !== 0 ? (
-          <p className={classes.Error}>{name.error}</p>
+        {input.name.hasError && input.name.touched ? (
+          <p className={classes.Error}>{input.name.error}</p>
         ) : null}
       </div>
 
@@ -203,12 +137,13 @@ const Auth = (props) => {
         <input
           placeholder="Email"
           type="email"
-          value={email.value}
-          onChange={emailChangedHandler}
+          name="email"
+          value={input.email.value}
+          onChange={inputChangedHandler}
         />
 
-        {email.error.length !== 0 ? (
-          <p className={classes.Error}>{email.error}</p>
+        {input.email.hasError && input.email.touched ? (
+          <p className={classes.Error}>{input.email.error}</p>
         ) : null}
       </div>
 
@@ -216,16 +151,17 @@ const Auth = (props) => {
         <input
           placeholder="Password"
           type="password"
-          value={password.value}
-          onChange={passwordChangedHandler}
+          name="password"
+          value={input.password.value}
+          onChange={inputChangedHandler}
         />
 
-        {password.error.length !== 0 ? (
-          <p className={classes.Error}>{password.error}</p>
+        {input.password.hasError && input.password.touched ? (
+          <p className={classes.Error}>{input.password.error}</p>
         ) : null}
       </div>
 
-      <Button primary login disabled={hasError}>
+      <Button primary login disabled={!formValid}>
         login
       </Button>
       {props.isLoading && <Spinner />}
